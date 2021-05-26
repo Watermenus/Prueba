241cb42ecdab2f9079b20706a0133a3f478fa302
Watermenus <84546860+Watermenus@users.noreply.github.com>
2021-05-25 19:18:24 -0600

    Cambio

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -1,1 +1,11 @@
 
+funcion recogerDatos(){
+	let nombreCliente = document.getElementById("name").value;
+	let emailCliente = document.getElementById("email").value;
+	let phoneCliente = document.getElementById("phone").value;
+	let peopleCliente = document.getElementById("people").value;
+	let messageCliente = document.getElementById("message").value;
+	let mensaje = "Nombre: " + nombreCliente + "\n" + "Email: " + emailCliente + "\n";
+
+	window.location.href = "mailto:jeaustin.rdz@gmail.com?subject=Reservacion&body=:" + mensaje;
+}
